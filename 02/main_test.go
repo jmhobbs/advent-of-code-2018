fcb44f3242140b3e6a43cd716677e732861f071b
John Hobbs <john@velvetcache.org>
2018-12-01 23:21:20 -0600

    Problem 2-A Test

---
+++ 02/main_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"testing"
+
+	h "github.com/jmhobbs/advent-of-code-2018/helpers"
+)
+
+type testCase struct {
+	String     string
+	TwoCount   bool
+	ThreeCount bool
+}
+
+var testCases []testCase = []testCase{
+	// abcdef contains no letters that appear exactly two or three times.
+	{
+		"abcdef",
+		false,
+		false,
+	},
+	// bababc contains two a and three b, so it counts for both.
+	{
+		"bababc",
+		true,
+		true,
+	},
+	// abbcde contains two b, but no letter appears exactly three times.
+	{
+		"abbcde",
+		true,
+		false,
+	},
+	// abcccd contains three c, but no letter appears exactly two times.
+	{
+		"abcccd",
+		false,
+		true,
+	},
+	// aabcdd contains two a and two d, but it only counts once.
+	{
+		"aabcdd",
+		true,
+		false,
+	},
+	// abcdee contains two e.
+	{
+		"abcdee",
+		true,
+		false,
+	},
+	// ababab contains three a and three b, but it only counts once.
+	{
+		"ababab",
+		false,
+		true,
+	},
+}
+
+func TestCount(t *testing.T) {
+	for _, c := range testCases {
+		two, three := count(c.String)
+		h.Equals(t, c.TwoCount, two)
+		h.Equals(t, c.ThreeCount, three)
+	}
+}
